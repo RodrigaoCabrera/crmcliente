678419dace494fb452cd589d6d87e5ae765bcb80
Rodrigo Cabrera <rodrigod33d@gmail.com>
2023-09-17 17:14:57 -0300

    DELETE: Código por defecto innecesario

---
+++ components/Siderbar.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Siderbar = () => {
+  return (
+    <aside className="bg-gray-800 min-h-screen sm:w-1/3 xl:w-1/5 sm:min-h-screen p-5">
+      <div>
+        <p className="text-white text-2xl font-black">crm clients</p>
+      </div>
+    </aside>
+  );
+};
+
+export default Siderbar;
