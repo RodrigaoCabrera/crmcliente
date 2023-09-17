e82fc2e1e50bbc3f2921aa7c97d5c2ac7e4e6d56
Rodrigo Cabrera <rodrigod33d@gmail.com>
2023-09-17 18:33:39 -0300

    ADD: Formik and yup for form manager

--- pages/register.js
+++ pages/register.js
@@ -1,13 +1,36 @@
 import React from "react";
 import Layout from "../components/Layout";
 
+// Formik
+import { useFormik } from "formik";
+import * as Yup from "yup";
+
 const Register = () => {
+  // Form validate
+  const formik = useFormik({
+    initialValues: {
+      name: "",
+      lastName: "",
+      email: "",
+      password: "",
+    },
+    validationSchema: Yup.object({
+      name: Yup.string().required("Name is required"),
+    }),
+    onSubmit: (values) => {
+      console.log("enviando");
+      console.log(values);
+    },
+  });
   return (
     <Layout>
       <h1 className="text-center text-2xl- text-white font-light">Register</h1>
       <div className="flex justify-center mt-5">
         <div className="w-full max-w-sm">
-          <form className="bg-white rounded shadow-md px-8 pt-6 pb-8 mb-4">
+          <form
+            className="bg-white rounded shadow-md px-8 pt-6 pb-8 mb-4"
+            onSubmit={formik.handleSubmit}
+          >
             <div className="mb-4">
               <label
                 className="block text-gray-700 text-sm font-bold mb-2"
@@ -20,8 +43,17 @@ const Register = () => {
                 className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
                 id="name"
                 placeholder="Name"
+                value={formik.values.name}
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
               />
             </div>
+            {formik.touched.name && formik.errors.name && (
+              <div className="my-2 bg-red-100 border-l-4 border-red-500 text-red-700 p-4">
+                <p className="font-bold">Error</p>
+                <p>{formik.errors.name}</p>
+              </div>
+            )}
             <div className="mb-4">
               <label
                 className="block text-gray-700 text-sm font-bold mb-2"
@@ -34,6 +66,8 @@ const Register = () => {
                 className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
                 id="lastName"
                 placeholder="LastName"
+                value={formik.values.lastName}
+                onChange={formik.handleChange}
               />
             </div>
             <div className="mb-4">
@@ -48,6 +82,8 @@ const Register = () => {
                 className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
                 id="email"
                 placeholder="Email address"
+                value={formik.values.email}
+                onChange={formik.handleChange}
               />
             </div>
             <div>
@@ -62,6 +98,8 @@ const Register = () => {
                 className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
                 id="password"
                 placeholder="Password"
+                value={formik.values.password}
+                onChange={formik.handleChange}
               />
             </div>
 
