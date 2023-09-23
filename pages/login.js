b6adbff1584861088f1a582ffc9026bdaad9635e
Rodrigo Cabrera <rodrigod33d@gmail.com>
2023-09-23 14:48:42 -0300

    ADD: user authentications

--- pages/login.js
+++ pages/login.js
@@ -1,13 +1,63 @@
 import React from "react";
 import Layout from "../components/Layout";
 
+// Formik
+import { useFormik } from "formik";
+import * as Yup from "yup";
+
+// Graphql
+import { useMutation, gql } from "@apollo/client";
+const USER_AUTHETICATOR = gql`
+  mutation authenticateUser($input: autenticateInput) {
+    authenticateUser(input: $input) {
+      token
+    }
+  }
+`;
+
 const login = () => {
+  // Mutation for create new users
+  const [authenticateUser] = useMutation(USER_AUTHETICATOR);
+
+  const formik = useFormik({
+    initialValues: {
+      email: "",
+      password: "",
+    },
+    validationSchema: Yup.object({
+      email: Yup.string()
+        .email("Email is not valid")
+        .required("Email is required"),
+      password: Yup.string().required("password is required"),
+    }),
+    onSubmit: async (values) => {
+      const { email, password } = values;
+      console.log(email, password);
+      try {
+        const { data } = await authenticateUser({
+          variables: {
+            input: {
+              email,
+              password,
+            },
+          },
+        });
+        console.log(data);
+      } catch (error) {
+        console.log(error);
+      }
+    },
+  });
+
   return (
     <Layout>
       <h1 className="text-center text-2xl- text-white font-light">login</h1>
       <div className="flex justify-center mt-5">
         <div className="w-full max-w-sm">
-          <form className="bg-white rounded shadow-md px-8 pt-6 pb-8 mb-4">
+          <form
+            className="bg-white rounded shadow-md px-8 pt-6 pb-8 mb-4"
+            onSubmit={formik.handleSubmit}
+          >
             <div className="mb-4">
               <label
                 className="block text-gray-700 text-sm font-bold mb-2"
@@ -20,8 +70,17 @@ const login = () => {
                 className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
                 id="email"
                 placeholder="Email address"
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+                value={formik.values.email}
               />
             </div>
+            {formik.touched.email && formik.errors.email && (
+              <div className="my-2 bg-red-100 border-l-4 border-red-500 text-red-700 p-4">
+                <p className="font-bold">Error</p>
+                <p>{formik.errors.email}</p>
+              </div>
+            )}
             <div>
               <label
                 className="block text-gray-700 text-sm font-bold mb-2"
@@ -34,8 +93,17 @@ const login = () => {
                 className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
                 id="password"
                 placeholder="Password"
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+                value={formik.values.password}
               />
             </div>
+            {formik.touched.password && formik.errors.password && (
+              <div className="my-2 bg-red-100 border-l-4 border-red-500 text-red-700 p-4">
+                <p className="font-bold">Error</p>
+                <p>{formik.errors.password}</p>
+              </div>
+            )}
 
             <input
               type="submit"
