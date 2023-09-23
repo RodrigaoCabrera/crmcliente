6be3d2e5d64b7ea253d2f321c6d5e9c8cf5e2711
Rodrigo Cabrera <rodrigod33d@gmail.com>
2023-09-23 16:12:46 -0300

    ADD: Query of user client

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,23 @@
 import Layout from "../components/Layout";
 
+// Graphql
+import { useQuery, gql } from "@apollo/client";
+
+const GET_USER_CLIENTS = gql`
+  query GetClientsSeller {
+    getClientsSeller {
+      name
+      lastName
+      email
+      telephone
+    }
+  }
+`;
 export default function Index() {
+  // Get from apollo
+
+  const { data, loading, error } = useQuery(GET_USER_CLIENTS);
+  console.log(data);
   return (
     <div>
       <Layout>
