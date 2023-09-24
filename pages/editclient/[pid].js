bd49489ce54106ed366c999dacbdedb9a6b21f0b
Rodrigo Cabrera <rodrigod33d@gmail.com>
2023-09-24 14:39:52 -0300

    ADD: Mutation for update client + swal alert and redirect to home

--- pages/editclient/[pid].js
+++ pages/editclient/[pid].js
@@ -1,13 +1,15 @@
 import React from "react";
 import { useRouter } from "next/router";
 import Layout from "../../components/Layout";
+// Alert message
+import Swal from "sweetalert2";
 
 // Formik
 import { Formik } from "formik";
 import * as Yup from "yup";
 
 // Graphql
-import { useQuery, gql } from "@apollo/client";
+import { useQuery, gql, useMutation } from "@apollo/client";
 
 const GET_CLIENT = gql`
   query GetClient($id: ID!) {
@@ -17,6 +19,15 @@ const GET_CLIENT = gql`
       telephone
       email
       business
+    }
+  }
+`;
+
+const UPDATE_CLIENT = gql`
+  mutation UpdateClient($id: ID!, $input: ClientInput) {
+    updateClient(id: $id, input: $input) {
+      name
+      lastName
     }
   }
 `;
@@ -31,6 +42,8 @@ const EditClient = () => {
   const { data, loading, error } = useQuery(GET_CLIENT, {
     variables: { id },
   });
+  // Get current client
+  const [updateClient] = useMutation(UPDATE_CLIENT);
 
   // Yup validation schema
   const schemaValidation = Yup.object({
@@ -43,14 +56,40 @@ const EditClient = () => {
   });
 
   if (loading) return <h1>Loading...</h1>;
+  const { getClient } = data;
+
+  // Upadte client in db
+  const handleUpdate = async (values) => {
+    console.log(values);
+    const { name, lastName, business, email, telephone } = values;
+    try {
+      const { data } = await updateClient({
+        variables: {
+          id,
+          input: { name, lastName, business, email, telephone },
+        },
+      });
+      // Sweet alert
+      Swal.fire("Update client", "Client updated successfully", "success");
+
+      // Redirect to home
+      router.push("/");
+    } catch (error) {
+      console.log(error);
+    }
+  };
   return (
     <Layout>
       <h1 className="text-2xl text-gray-800 font-light ">Edit client</h1>
       <div className="flex justify-center mt-5">
         <div className="w-full max-w-lg">
-          <Formik validationSchema={schemaValidation}>
+          <Formik
+            validationSchema={schemaValidation}
+            enableReinitialize
+            initialValues={getClient}
+            onSubmit={(values) => handleUpdate(values)}
+          >
             {(props) => {
-              console.log(props);
               return (
                 <form
                   className="bg-white shadow-md px-8 pt-6 pb-8 mb-4"
@@ -71,7 +110,7 @@ const EditClient = () => {
                       placeholder="Client name"
                       onChange={props.handleChange}
                       onBlur={props.handleBlur}
-                      //value={props.values.name}
+                      value={props.values.name}
                     />
                   </div>
                   {props.touched.name && props.errors.name && (
@@ -96,7 +135,7 @@ const EditClient = () => {
                       placeholder="Client last name"
                       onChange={props.handleChange}
                       onBlur={props.handleBlur}
-                      //value={props.values.lastName}
+                      value={props.values.lastName}
                     />
                   </div>
                   {props.touched.lastName && props.errors.lastName && (
@@ -121,7 +160,7 @@ const EditClient = () => {
                       placeholder="Client business"
                       onChange={props.handleChange}
                       onBlur={props.handleBlur}
-                      //value={props.values.business}
+                      value={props.values.business}
                     />
                   </div>
                   {props.touched.business && props.errors.business && (
@@ -146,7 +185,7 @@ const EditClient = () => {
                       placeholder="Client email address"
                       onChange={props.handleChange}
                       onBlur={props.handleBlur}
-                      //value={props.values.email}
+                      value={props.values.email}
                     />
                   </div>
                   {props.touched.email && props.errors.email && (
@@ -158,26 +197,26 @@ const EditClient = () => {
                   <div className="mb-4">
                     <label
                       className="block text-gray-700 text-sm font bold mb-2"
-                      htmlFor="phone"
+                      htmlFor="telephone"
                     >
-                      Phone
+                      Telephone
                     </label>
 
                     <input
                       type="tel"
                       className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700"
-                      id="phone"
-                      placeholder="Client phone address"
+                      id="telephone"
+                      placeholder="Client telephone address"
                       onChange={props.handleChange}
                       onBlur={props.handleBlur}
-                      //value={props.values.phone}
+                      value={props.values.telephone}
                     />
                   </div>
 
                   <input
                     type="submit"
                     className="bg-gray-800 w-full mt-5 p-2 text-white uppercase font-bold hover:bg-gray-900"
-                    //value="Registrar cliente"
+                    value="Editar cliente"
                   />
                 </form>
               );
