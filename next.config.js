62cfd7828370012c19adc3be7d4e24c8f876c0d1
Rodrigo Cabrera <rodrigod33d@gmail.com>
2023-09-30 23:00:00 -0300

    ADD: new config in next to allow graphql files

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+module.exports = {
   reactStrictMode: true,
-}
+  webpack: (config, options) => {
+    config.module.rules.push({
+      test: /\.(graphql|gql)$/,
+      exclude: /node_modules/,
+      loader: "graphql-tag/loader",
+    });
 
-module.exports = nextConfig
+    return config;
+  },
+};
